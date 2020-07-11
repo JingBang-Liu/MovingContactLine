f2cb949a0f0e4b8bc691668379d6215ddd098bd5
sxbkdljb <prcdavidl@163.com>
2020-07-12 00:47:29 +0100

    JL: new

--- analysis/write_netcdf.f90
+++ analysis/write_netcdf.f90
@@ -20,36 +20,75 @@ MODULE write_netcdf
     ! dimension ids for output variable
     INTEGER, DIMENSION(ndims) :: dim_ids_bins
     ! variable ids for output variable
-    INTEGER, DIMENSION(ndims) :: var_id_bins
+    INTEGER :: var_id_bins
     ! file id
     INTEGER :: file_id
     ! for loop
-    INTEGER :: i
+    INTEGER :: i, ierr
     
     ! shape of bins
     sizes_bins = SHAPE(bins)
 
     ! create netCDF file
-    CALL check(nf90_create(filename_bins, nf90_clobber, file_id))
+    !CALL check(nf90_create(filename_bins, nf90_clobber, file_id))
     ! add information about global runtime data
-    CALL ckeck(nf90_put_att(file_id, NF90_BLOBAL, 'time_marks', time_marks))
+    !CALL ckeck(nf90_put_att(file_id, NF90_GLOBAL, 'time_marks', time_marks))
 
     ! define dimension ids
-    DO i=1, ndims
-      CALL check(nf90_def_dim(file_id, dim_bins(i), sizes_bins(i), dim_ids_bins(i)))
-    END DO
+    !DO i=1, ndims
+    !  CALL check(nf90_def_dim(file_id, dims_bins(i), sizes_bins(i), dim_ids_bins(i)))
+    !END DO
 
     ! define variable ids
-    CALL check(nf90_def_var(file_id, "bins", NF90_DOUBLE, dim_ids_bins, var_id_bins))
+    !CALL check(nf90_def_var(file_id, "bins", NF90_DOUBLE, dim_ids_bins, var_id_bins))
 
     ! end define
-    CALL check(nf90_enddef(file_id))
+    !CALL check(nf90_enddef(file_id))
 
     ! write data
-    CALL check(nf90_put_var(file_id, var_id_bins, bins))
+    !CALL check(nf90_put_var(file_id, var_id_bins, bins))
 
     ! close netCDF file
-    CALL check(nf90_close(file_id))
+    !CALL check(nf90_close(file_id))
+
+    ierr = nf90_create(filename_bins, NF90_CLOBBER, file_id)
+
+    IF (ierr /= nf90_noerr) THEN
+      PRINT*, TRIM(nf90_strerror(ierr))
+      RETURN
+    END IF
+
+    DO i=1,ndims
+      ierr = nf90_def_dim(file_id, dims_bins(i), sizes_bins(i), dim_ids_bins(i))
+      IF (ierr /= nf90_noerr) THEN
+        PRINT*, TRIM(nf90_strerror(ierr))
+        RETURN
+      END IF
+    END DO
+
+    ierr = nf90_def_var(file_id, "bins", NF90_DOUBLE, dim_ids_bins, var_id_bins)
+    IF (ierr /= nf90_noerr) THEN
+      PRINT*, TRIM(nf90_strerror(ierr))
+      RETURN
+    END IF
+    
+    ierr = nf90_enddef(file_id)
+    IF (ierr /= nf90_noerr) THEN
+      PRINT*, TRIM(nf90_strerror(ierr))
+      RETURN
+    END IF
+
+    ierr = nf90_put_var(file_id, var_id_bins, bins)
+    IF (ierr /= nf90_noerr) THEN
+      PRINT*, TRIM(nf90_strerror(ierr))
+      RETURN
+    END IF
+
+    ierr = nf90_close(file_id)
+    IF (ierr /= nf90_noerr) THEN
+      PRINT*, TRIM(nf90_strerror(ierr))
+      RETURN
+    END IF
   END SUBROUTINE
 
   SUBROUTINE check(stat)
